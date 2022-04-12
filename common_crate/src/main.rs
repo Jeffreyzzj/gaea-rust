399176fda6252f930c276de4213d226dd8c5dc2d
zijie.zeng <jeffreyzzj1996@163.com>
2022-04-12 14:57:27 +0800

    zijie.zeng init gaea

--- common_crate/src/main.rs
+++ common_crate/src/main.rs
@@ -1,3 +1,9 @@
+use log::info;
+use log4rs;
+
 fn main() {
-    println!("Hello, world!");
+    //println!("Hello, world!");
+    //初始化日志信息
+    log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
+    
 }
