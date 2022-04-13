b7fbe1768feb88745d24bfa67d7630e18db5e62c
zijie.zeng <jeffreyzzj1996@163.com>
2022-04-13 20:58:28 +0800

    zijie.zeng add lib

--- gaea_trie_tree/src/lib.rs
+++ gaea_trie_tree/src/lib.rs
@@ -18,29 +18,26 @@ use service::file_uitl::{FileUtil};
 // }
 
 lazy_static! {
-    static ref config_tree: RwLock<ConfigTree> = RwLock::new(ConfigTree::new(test_get_str()));
+    static ref CONFIG_TREE: RwLock<ConfigTree> = RwLock::new(ConfigTree::new("".to_string()));
 
-    static ref id_char_map: HashMap<i32, String> = FileUtil::init_id_char_map();
+    static ref ID_CHAR_MAP: HashMap<i32, String> = FileUtil::init_id_char_map();
 
-    static ref char_id_map: HashMap<String, i32> = FileUtil::init_char_id_map();
-}
-
-pub fn test_get_str() -> String {   
-    String::from("abcccc")
+    static ref CHAR_ID_MAP: HashMap<String, i32> = FileUtil::init_char_id_map();
 }
 
 pub fn init_config(path: String) {
-    
-    write_str(path);
-    //read_conf();
+    // 重写路径
+    re_write_config_path(path);
+    // 读取数据
+    get_tree_conf();
 }
 
-pub fn read_conf() {
-    let aa = config_tree.read().unwrap().to_owned();
-    println!("hahaha {:?}", aa);
+pub fn get_tree_conf() {
+    let config_info = CONFIG_TREE.read().unwrap().to_owned();
+    println!("config: \n{:?}", config_info);
 }
 
-pub fn write_str(path: String) {
-    let mut new_config_tree = config_tree.write().unwrap();
+pub fn re_write_config_path(path: String) {
+    let mut new_config_tree = CONFIG_TREE.write().unwrap();
     *new_config_tree = ConfigTree::new(path);
 }
